4ae6d8d7ccaeddea6831274fad73795fbbac5f2e
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-17 18:44:50 +0000

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints a quote in stderr
+ * Return: Always 1 
+ */
+
+int main(void)
+{
+	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	
+	return (1);
+
+}
