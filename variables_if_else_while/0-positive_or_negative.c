8e2752e80ddd4fd41705345f63829e2d63e989f2
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-21 03:26:23 +0000

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 
-int main(if (n=0))
-{
-	prinf"is zero \n";
 
-	else if (n>0)
+
+	if (n=0)
 	{
-		printf"is positive \n";
+		prinf"is zero \n";
 
-		else if (n<0)
+		else if (n>0)
 		{
-			printf"is negative \n";
+			printf"is positive \n";
+
+			else if (n<0)
+			{
+				printf"is negative \n";
+			}
 
 		}
 	}
-}
+
 
