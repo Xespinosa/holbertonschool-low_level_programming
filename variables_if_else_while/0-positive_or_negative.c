856e953b997dc5d48a38e6b008cc0f7db4b2d9b2
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-21 14:07:37 +0000

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - print whether a random number is positive, negative or zero
- * return: 0 
+ * return: 0
  */
 int main(void)
 {
@@ -12,18 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 	{
-		 printf ("%d is positive\n", n);
-	}  
-			else if(n < 0)
-			{
-				printf ("%d is negative\n", n);
-			}
-				else 
-				{
-					printf ("%d is zero\n", n);
-				}
+		printf ("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf ("%d is negative\n", n);
+	}
+	else
+	{
+		printf ("%d is zero\n", n);
+	}
 	return (0);
 }
 
