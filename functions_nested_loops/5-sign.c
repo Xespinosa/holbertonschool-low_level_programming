b4462ab63ea17b1fcb8cad1be0919576d6643c75
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-22 20:16:25 +0000

    postive, negative or zero\?

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - positive or negative?
+ * @n: number to be avaluated
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+	else
+	{
+		_putchar (0);
+
+		return (0);
+	}
+}
