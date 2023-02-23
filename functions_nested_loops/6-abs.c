3b401fced12ab401f7c822241412054bd784ac00
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-23 14:37:14 +0000

    give me the absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute value
+ * Return: the absolute value
+ */
+
+int _abs(int s)
+{
+	if (s > 0)
+		return (s);
+	else 
+		return (s*-1);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,5 @@
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int *_putchar(char k);
+
+int _putchar(char k);
 
 void print_alphabet(void);
 
@@ -17,3 +11,4 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int s);
