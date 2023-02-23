946b6c3f0b37c9efa01471e34debfe0af295184a
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-23 21:21:14 +0000

    times table 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,8 +13,21 @@ void times_table(void)
 	for (y = 0; y <= 9; y++)
 	{
 		z = x * y;
+		if (z <= 9)
+		{
 		_putchar(z + '0');
-	}
+		_putchar(',');
+		_putchar(' ');
+		}
+		else
+		{
+			_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+
 	}
 	_putchar('\n');
+	}
 }
