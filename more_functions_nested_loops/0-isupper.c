63a9f1909de18aa73aaa44cfccd6123919deb1bc
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-25 23:46:32 +0000

    is upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppys
+ * @c: to be evaluated
+ * Return: 1 for upper 0 for else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
