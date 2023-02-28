692e74f4ac9b7e38c71069adff0678bb419d2499
Xai Betancourt <betancourtxavier77@gmail.com>
2023-02-28 17:15:57 +0000

    string length print npw in reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string without printing it
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int temp;
+	int len = 0;
+
+        while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
