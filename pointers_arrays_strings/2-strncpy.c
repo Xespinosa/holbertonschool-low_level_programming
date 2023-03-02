dea69cd9c936a72d31c9464f4265516be457b643
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-02 18:26:36 +0000

    copy a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		j++;
 	}
-        for (i = 0; i < n && src[i] != '\0'; i++)
+        for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
