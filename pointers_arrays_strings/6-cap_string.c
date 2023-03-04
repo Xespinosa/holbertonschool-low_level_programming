01dcbfea022d30379b208a212739f56643664629
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-04 08:51:15 +0000

    capitalize every word

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,14 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i]; i++)
 	{
-		while (!(str[i] >= 'a'; && str[i] <= 'z'))
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
 			i++;
 		}
 		if (str[i - 1] == ' ' || str[i - 1] == '\t'|| str[i - 1] == '\n' ||
 			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-			str[i - 1] == '1' || str[I - 1] == '?' || str[I - 1] == '"' ||
-			str[I - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '1' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
 			str[i - 1] == '}' || i == 0)
 		{
 			str[i] -= 32;
