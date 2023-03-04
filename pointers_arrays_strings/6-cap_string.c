5fbc30192023070e793e95c44f03eeb4c00b6bda
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-04 08:52:14 +0000

    capitalize every word

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *str)
 		{
 			i++;
 		}
-		if (str[i - 1] == ' ' || str[i - 1] == '\t'|| str[i - 1] == '\n' ||
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
 			str[i - 1] == '1' || str[i - 1] == '?' || str[i - 1] == '"' ||
 			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
