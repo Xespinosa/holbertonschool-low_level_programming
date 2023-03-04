22e5803022723f88d317c50119d05e385965d66a
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-04 08:07:19 +0000

    change lowercase to uppercase in array

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0, len = 0;
 
+	while (src[i++])
+	{
+		len++;
+	}
 	for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
+	}
+	for (i = len; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 
 	return (dest);
