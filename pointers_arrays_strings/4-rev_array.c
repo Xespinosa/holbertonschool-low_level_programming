ad1dae5c2c27a4c5835ab4dd1108aedcc8389aa3
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-04 07:44:01 +0000

    swap places along an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - swap the positions of the values in an array by half
+ * @a: array to be used
+ * @n: total length of the array
+ */
+
+void revers_array(int *a, int n)
+{
+	int i, j;
+
+	for (j = n - 1; j>= n / 2; j--)
+	{
+		i = a[n - 1 - j];
+		a[n - 1 - j] = a[j];
+		a[j] = i;
+	}
+}	
