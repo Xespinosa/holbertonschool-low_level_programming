9187e24f07feeda06fa85beb2877361d5a365525
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-08 13:37:08 +0000

    program to find the square root of a numbe

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * find_root - find the square root of a number
+ * @k: number to arbitrarily test against n
+ * @n: number for which to find the square root
+ * Return: return the square root of a number
+ */
+
+int find_root(int k, int n)
+{
+	if ( k * k > n)
+	{
+		return (-1)
+	}
+	if (k * k == n)
+	{
+		return (r)
+	}
+
+	return (find_root(n, k + 1);
+}
+
+/**
+ * _sqrt_recursion - find square root 
+ * @n: number to find the square root for
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (find_root(n, 0);
+}
