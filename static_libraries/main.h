b11a8c8f3745df710978801c986cea5e50297d93
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-08 15:52:36 +0000

    static libraries

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 
 int _islower(int c);
@@ -38,3 +39,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+#endif
