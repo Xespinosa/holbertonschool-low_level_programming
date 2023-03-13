20cb811644f1a5528ee45f9af0d270c18712a13c
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-13 15:06:55 +0000

    creating an array of variable size using malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - create a variable array using malloc
+ * @size: variable size of the array
+ * @c: character to be placed within the array
+ * Return: NULL if failure or pointer if success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar = malloc(size * sizepf(char));
+	int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (; i < size; i++)
+		ar[i] = c;
+
+	return (ar);
+}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+Found herein are the contents of every project pertaining to malloc and free
