9823b19fb0124c6deb1e60ddbc421f2beffeb1e7
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-13 16:48:43 +0000

    duplicate an array with the same size

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - suplicate the contents of a string
+ * *str: the string to be duplicated
+ * Return: NULL on failure or pointer to the string on success
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len;
+	int *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	if (dup == NULL)
+		return (NULL);
+
+	len = strlen(str) + 1;
+	dup = malloc(len);
+
+	for (; i <= len; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	return (dup);
+}
