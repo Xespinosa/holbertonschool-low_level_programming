38efa1539a99a755c0b0ef0b1aafa3b8aca08a26
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-20 14:29:18 +0000

    strcture practice

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - this is a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: this is a theoretical
+ * quadrapedal mammalian known
+ * for it's companionship
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
