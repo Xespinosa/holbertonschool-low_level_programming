0cfd6a12012b0a2676b5dbeae7a53ba79bd3889f
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-20 16:36:38 +0000

    initialize a structure

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize dog
+ * @d: dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d;
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+
+}
