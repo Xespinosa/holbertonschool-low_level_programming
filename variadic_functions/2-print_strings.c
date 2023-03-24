c43200e00efee858bc98bc07871d0bfd091e3a9c
Xai Betancourt <betancourtxavier77@gmail.com>
2023-03-24 15:29:39 +0000

    variadic functions

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(kp, char *) == NULL)
+		if (kp == NULL)
 			printf("(nil)");
 
 		printf("%s", va_arg(kp, char *));
