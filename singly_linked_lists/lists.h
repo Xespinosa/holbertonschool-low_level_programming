8b55fdc56ec3ef5f838e5d3d3ce4385259dc988f
Xai Betancourt <betancourtxavier77@gmail.com>
2023-04-05 02:56:38 +0000

    update to lists.h file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,6 +7,15 @@
 
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
 typedef struct list_s
 {
 char *str;
