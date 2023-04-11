8c877d5d651c46bda8a117e76f24e7d849744b0a
Xai Betancourt <betancourtxavier77@gmail.com>
2023-04-11 18:43:03 +0000

    update to file 0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - change a binary number to decimal
+ N* binary_to_uint - change a binary number to decimal
  * @b: binary number
  * Return: the changed number
  */
@@ -11,6 +11,8 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 	unsigned int k = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[i] != '\0')
 	{
 		if (b[i] == '1' || b[i] == '0')
